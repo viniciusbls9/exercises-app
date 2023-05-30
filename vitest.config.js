0033320b26a959e38d8d24e48b7fb28e0a75d83b
Vinicius <beneditoloura@gmail.com>
2023-05-30 07:05:26 -0300

    test: config test

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: ['test/setup.ts'],
+    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache']
   },
 })
