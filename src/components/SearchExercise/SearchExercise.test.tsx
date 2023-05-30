8294df218b46392cd9de3541f655d56c6325d518
Vinicius <beneditoloura@gmail.com>
2023-05-30 08:42:17 -0300

    test: create SearchExercise component test

---
+++ src/components/SearchExercise/SearchExercise.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import ExerciseSearch from './';
+
+describe('ExerciseSearch', () => {
+  it('renders the text and input field', () => {
+    render(<ExerciseSearch />);
+    const textElement = screen.getByText('Find your exercises');
+    const inputElement = screen.getByPlaceholderText('Enter your exercise');
+    expect(textElement).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+  });
+});
